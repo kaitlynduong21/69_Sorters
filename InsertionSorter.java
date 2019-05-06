f3bda6ea3816001e78727a1de972a39c658ec48d
kduong@stuy.edu <kduong@stuy.edu>
2019-05-06 14:05:47 -0400

    finished InsertionSort

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -10,14 +10,43 @@ public class InsertionSorter extends Sorter {
       Construct an instance to process the user's data
      */
     public InsertionSorter(  ArrayList< String> usersData) {
+        super(usersData);
         // your code here, if necessary
     }
-    
+
 
     /**
       sort the user's data, implementing insertion sort
      */
     public void mySort() {
-        // your code here
+      for (int goodNameNeeded = 1; goodNameNeeded < elements.size() - 1; goodNameNeeded++) {
+        insert1(goodNameNeeded);
+        /*System.out.println( "    dbg: "
+          + "after inserting element " + goodNameNeeded
+          + " elements: " + elements
+          );*/
+      }
     }
+
+    private void insert1(int alreadyInserted) {
+      String valueToInsert = elements.get(alreadyInserted);
+      for (int indexToCompare = alreadyInserted - 1; indexToCompare >= 0; indexToCompare -- ) {
+        if (valueToInsert.compareTo(elements.get(indexToCompare)) < 0) {
+          shift(indexToCompare);
+          if (indexToCompare == 0) dropValue(0, valueToInsert);
+        } else {
+          dropValue(indexToCompare + 1, valueToInsert);
+          indexToCompare = -1;
+        }
+      }
+    }
+
+    private void shift(int indexToMoveTo) {
+      elements.set(indexToMoveTo + 1, elements.get(indexToMoveTo));
+    }
+
+    private void dropValue(int indexToMoveTo, String valueToInsert) {
+      elements.set(indexToMoveTo, valueToInsert);
+    }
+
 }
--- Sorter.java
+++ Sorter.java
@@ -1,29 +1,29 @@
 /** 
-  represent an abstraction of sorting an array  
-  
+  represent an abstraction of sorting an array
+
  */
 import java.util.ArrayList;
 
 abstract class Sorter {
     protected ArrayList< String> elements;
-    
+
     /**
-      Construct an instance, saving a reference to the 
+      Construct an instance, saving a reference to the
       user's data
      */
     public Sorter( ArrayList< String> usersData) {
         elements = usersData;
     }
-    
 
-    /** 
+
+    /**
       Sort elements, using the algorithm of the subclass.
       Avoid name collision with ArrayList's sort() method.
      */
     public abstract void mySort();
 
 
-    /** 
+    /**
       @return a String representation of the elements
      */
     public String toString() {
@@ -34,7 +34,7 @@ abstract class Sorter {
     }
 
 
-    /** 
+    /**
       @return the boolean value of the statement
          "the elements are in ascending order"
      */
